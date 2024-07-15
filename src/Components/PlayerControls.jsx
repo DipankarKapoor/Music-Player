d746f6e134c3f40f59ab1c266bb2dabb31ad5fbb
DipankarKapoor <137331953+DipankarKapoor@users.noreply.github.com>
2024-07-16 01:31:20 +0530

    Update PlayerControls.jsx

--- src/Components/PlayerControls.jsx
+++ src/Components/PlayerControls.jsx
@@ -5,9 +5,9 @@ const PlayerControls = ({ isPlaying, onPlayPause, onNext, onPrevious }) => (
   <div className="playbar">
     <div className="playbar-icons"><img src='../public/group.svg' /></div>
     <div className="player-controls">
-      <img src='previous.svg' onClick={onPrevious} />
+      <img src='../public/previous.svg' onClick={onPrevious} />
       {isPlaying ? <FaPause onClick={onPlayPause} /> : <FaPlay onClick={onPlayPause} />}
-      <img src='next.svg' onClick={onNext} />
+      <img src='../public/next.svg' onClick={onNext} />
     </div>
     <div className="playbar-icons"><img src='../public/volume.svg' /></div>
   </div>
