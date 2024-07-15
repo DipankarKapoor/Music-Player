84679a27db653b8d95279985ec9e6b1da909468e
DipankarKapoor <137331953+DipankarKapoor@users.noreply.github.com>
2024-07-16 01:58:40 +0530

    Update PlayerControls.jsx

--- src/Components/PlayerControls.jsx
+++ src/Components/PlayerControls.jsx
@@ -3,13 +3,13 @@ import { FaPlay, FaPause } from 'react-icons/fa';
 
 const PlayerControls = ({ isPlaying, onPlayPause, onNext, onPrevious }) => (
   <div className="playbar">
-    <div className="playbar-icons"><img src='../public/group.svg' /></div>
+    <div className="playbar-icons"><img src='./group.svg' /></div>
     <div className="player-controls">
-      <img src='../public/previous.svg' onClick={onPrevious} />
+      <img src='./previous.svg' onClick={onPrevious} />
       {isPlaying ? <FaPause onClick={onPlayPause} /> : <FaPlay onClick={onPlayPause} />}
-      <img src='../public/next.svg' onClick={onNext} />
+      <img src='./next.svg' onClick={onNext} />
     </div>
-    <div className="playbar-icons"><img src='../public/volume.svg' /></div>
+    <div className="playbar-icons"><img src='./volume.svg' /></div>
   </div>
 );
 
